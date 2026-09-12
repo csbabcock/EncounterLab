5e09082d01b2e5ceb2ef2c280bc345ea7b560cd9
VULTRON <csbabcock@protonmail.com>
2026-09-12 08:35:07 -0400

    Adding bare bones for avg damage calc

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void showAverageDamage(double avgDamage)
+{
+    std::cout << "Average damage: " << avgDamage << std::endl;
+}
+
+double calculateAverageDamage(int numDice, int numDiceSides, int damageBonus)
+{
+    return (1 + numDiceSides) / 2.0 * numDice + damageBonus;
+}
+
+void getDamage()
+{
+    int numDice{};
+    int numDiceSides{};
+    int damageBonus{};
+
+    std::cout << "Number of dice: ";
+    std::cin >> numDice;
+
+    std::cout << "Number of dice sides: ";
+    std::cin >> numDiceSides;
+
+    std::cout << "Damage bonus: ";
+    std::cin >> damageBonus;
+
+    double avgDamage{calculateAverageDamage(numDice, numDiceSides, damageBonus)};
+    showAverageDamage(avgDamage);
+
+}
+
+int main()
+{
+    getDamage();
     return 0;
 }
